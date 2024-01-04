5d99811c6f8370eba6c6c5ea09e14144444fb7dc
Mohammad222PR <mohammades13851@gmail.com>
2024-01-05 00:51:45 +0330

    commit

--- core/blog/admin/blog.py
+++ core/blog/admin/blog.py
@@ -1,2 +1,13 @@
 from django.contrib import admin
 from blog.models import *
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    '''Admin View for Blog'''
+    list_display = ('author','title','body','created_time','updated_time' ,'slug', 'category')
+    list_filter = ('created_time','updated_time', 'category','tag')
+    readonly_fields = ('author','title','body','created_time','updated_time' ,'slug', 'category',)
+    search_fields = ('author','title','body' ,'slug', 'category')
+    ordering = ('created_time','updated_time' , 'category',)
+    prepopulated_fields = {'slug':('title',)}
